8d58c6ed39f113f87badf7e2d1de901edf232bf7
Manzy <3236583972@qq.com>
2024-07-27 17:39:00 +0800

    vitest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+  },
+  test: {
+		reporters: ['default', 'html'],
+		coverage: {
+			enabled: true,
+			provider: 'v8',
+			cleanOnRerun: true,
+			reporter: ['text', 'json', 'html'],
+		},
+	},
 })
